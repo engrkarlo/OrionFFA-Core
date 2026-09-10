6d2d92ce076c00f650af4765bbba91f47f085376
Karlo_W <torrescarl328@gmail.com>
2026-09-10 14:46:16 +0800

    Extend schematic adapter for arena creation

--- src/main/java/com/karlo/orionffa/arena/SchematicService.java
+++ src/main/java/com/karlo/orionffa/arena/SchematicService.java
@@ -1,14 +1,28 @@
 package com.karlo.orionffa.arena;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import java.io.File;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
-@FunctionalInterface
 public interface SchematicService {
     void paste(File schematic, Location target) throws Exception;
 
     default boolean asyncCapable() {
         return false;
+    }
+
+    default boolean giveSelectionWand(Player player) {
+        return false;
+    }
+
+    default Optional<ArenaSelection> captureSelection(Player player) {
+        return Optional.empty();
+    }
+
+    default CompletableFuture<Void> saveSelection(Player player, File schematic) {
+        return CompletableFuture.failedFuture(new UnsupportedOperationException("Selection saving is unavailable"));
     }
 }
