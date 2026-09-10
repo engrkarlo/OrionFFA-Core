2e83549905aa8cb18bd88473cff8ce62a07615d2
Karlo_W <torrescarl328@gmail.com>
2026-09-10 14:00:19 +0800

    Prepare schematic adapter for async-capable FAWE resets

--- src/main/java/com/karlo/orionffa/arena/SchematicService.java
+++ src/main/java/com/karlo/orionffa/arena/SchematicService.java
@@ -1,4 +1,14 @@
 package com.karlo.orionffa.arena;
-import com.karlo.orionffa.config.LocationConfig;
+
+import org.bukkit.Location;
+
 import java.io.File;
-@FunctionalInterface public interface SchematicService { void paste(File schematic, LocationConfig target) throws Exception; }
+
+@FunctionalInterface
+public interface SchematicService {
+    void paste(File schematic, Location target) throws Exception;
+
+    default boolean asyncCapable() {
+        return false;
+    }
+}
